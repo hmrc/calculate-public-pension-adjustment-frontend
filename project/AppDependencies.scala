7089d9d0fc8e046e7833a015c567e6d4d03f0962
Dan Corbin <125590758+dacohm@users.noreply.github.com>
2023-05-04 16:59:04 +0100

    MCSC-284 uplift hmrc compile dependency versions and align runtime of typesafe play sbt-plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay28Version = "6.4.0"
+  private val bootstrapPlay28Version = "7.14.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.22.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "6.8.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootstrapPlay28Version,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.68.0",
-    "uk.gov.hmrc"       %% "url-builder"                   % "3.8.0-play-28"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "1.1.0"
   )
 
   val test = Seq(
@@ -25,7 +24,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"               % PlayVersion.current,
     "org.mockito"            %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"         %% "scalacheck"              % "1.15.4",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.68.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "1.1.0",
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
 
