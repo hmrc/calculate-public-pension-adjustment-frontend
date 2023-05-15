cf880fb1c3a834d5d5cc35beb52a33ab9e975058
ElliotAndersonHMRC <114657325+ElliotAndersonHMRC@users.noreply.github.com>
2023-05-15 12:19:26 +0100

    Finished Unit tests

--- test/controllers/ReportingChangeControllerSpec.scala
+++ test/controllers/ReportingChangeControllerSpec.scala
@@ -18,11 +18,12 @@ package controllers
 
 import base.SpecBase
 import forms.ReportingChangeFormProvider
+import models.ReportingChange.{LifetimeAllowance, OtherCompensation}
 import models.{CheckMode, NormalMode, ReportingChange, UserAnswers, WhichYearsScottishTaxpayer}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.{ReportingChangePage, WhichYearsScottishTaxpayerPage}
+import pages.{ReportingChangePage, ScottishTaxpayerFrom2016Page, WhichYearsScottishTaxpayerPage}
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -139,6 +140,138 @@ class ReportingChangeControllerSpec extends SpecBase with MockitoSugar {
         redirectLocation(result).value mustEqual ReportingChangePage
           .navigate(CheckMode, expectedAnswers)
           .url
+      }
+    }
+
+    "must redirect to ScottishTaxpayerFrom2016 when user selects AnnualAllowance in Normal Mode" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, reportingNormalRoute)
+            .withFormUrlEncodedBody(("value[0]", ReportingChange.AnnualAllowance.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.ScottishTaxpayerFrom2016Controller.onPageLoad(NormalMode).url
+      }
+    }
+
+    "must redirect to CheckYourAnswers when user does not select AnnualAllowance in Normal Mode" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, reportingNormalRoute)
+            .withFormUrlEncodedBody(("value[0]", ReportingChange.LifetimeAllowance.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.CheckYourAnswersController.onPageLoad.url
+      }
+    }
+
+    "must redirect to CheckYourAnswers when user is in CheckMode and answers exist for ScottishTaxpayerFrom2016Page" in {
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val answers = emptyUserAnswers
+        .set(ReportingChangePage, ReportingChange.values.toSet).success.value
+        .set(ScottishTaxpayerFrom2016Page, true).success.value
+
+      val application =
+        applicationBuilder(userAnswers = Some(answers))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, reportingCheckRoute)
+            .withFormUrlEncodedBody(("value[0]", ReportingChange.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.CheckYourAnswersController.onPageLoad.url
+      }
+    }
+
+    "must redirect to ScottishTaxpayerFrom2016 when user is in CheckMode and answers do not exist for ScottishTaxpayerFrom2016Page" in {
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val answers = emptyUserAnswers
+        .set(ReportingChangePage, ReportingChange.values.toSet).success.value
+
+      val application =
+        applicationBuilder(userAnswers = Some(answers))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, reportingCheckRoute)
+            .withFormUrlEncodedBody(("value[0]", ReportingChange.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.ScottishTaxpayerFrom2016Controller.onPageLoad(CheckMode).url
+      }
+    }
+
+    "must redirect to CheckYourAnswers when user is in CheckMode and user doesn't select annual allowance" in {
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val answers = emptyUserAnswers
+        .set(ReportingChangePage, ReportingChange.values.toSet).success.value
+
+      val application =
+        applicationBuilder(userAnswers = Some(answers))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, reportingCheckRoute)
+            .withFormUrlEncodedBody(("value[0]", ReportingChange.LifetimeAllowance.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.CheckYourAnswersController.onPageLoad.url
       }
     }
 
