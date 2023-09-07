921082f4a64bcd6c0e52e4adff14a99a3369dc1a
Bharti Verma <68339135+bharti5verma@users.noreply.github.com>
2023-09-07 10:34:32 +0100

    MCSC-466 Create Value Taken Pages

--- app/models/ExcessLifetimeAllowancePaid.scala
+++ app/models/ExcessLifetimeAllowancePaid.scala
@@ -34,12 +34,20 @@ object ExcessLifetimeAllowancePaid extends Enumerable.Implicits {
     Both
   )
 
-  def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map { case (value, index) =>
-    RadioItem(
-      content = Text(messages(s"excessLifetimeAllowancePaid.${value.toString}")),
-      value = Some(value.toString),
-      id = Some(s"value_$index")
+  def options(implicit messages: Messages): Seq[RadioItem] = {
+    val normalOptions = values.zipWithIndex.map { case (value, index) =>
+      RadioItem(
+        content = Text(messages(s"excessLifetimeAllowancePaid.${value.toString}")),
+        value = Some(value.toString),
+        id = Some(s"value_$index")
+      )
+    }
+    val orOption      = RadioItem(
+      id = Some(messages("divider")),
+      disabled = true,
+      divider = Some(messages("divider"))
     )
+    normalOptions.slice(0, 2) ++ Seq(orOption) ++ normalOptions.slice(2, normalOptions.length)
   }
 
   implicit val enumerable: Enumerable[ExcessLifetimeAllowancePaid] =
