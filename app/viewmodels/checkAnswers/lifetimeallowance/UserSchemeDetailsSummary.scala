19e6d3cb512cb0e2535d0af040dd2e460ccc757c
Bharti Verma <68339135+bharti5verma@users.noreply.github.com>
2023-09-20 21:47:13 +0100

    MCSC-467 Rebased with main branch

--- app/viewmodels/checkAnswers/lifetimeallowance/UserSchemeDetailsSummary.scala
+++ app/viewmodels/checkAnswers/lifetimeallowance/UserSchemeDetailsSummary.scala
@@ -16,7 +16,6 @@
 
 package viewmodels.checkAnswers.lifetimeallowance
 
-import controllers.routes
 import models.{CheckMode, UserAnswers}
 import pages.lifetimeallowance.UserSchemeDetailsPage
 import play.api.i18n.Messages
