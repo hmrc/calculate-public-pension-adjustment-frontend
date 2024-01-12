a35ca713c69b4714d58f9ee84b1a7dff4ebb5cc3
Elliot Anderson <114657325+ElliotAndersonHMRC@users.noreply.github.com>
2024-01-12 14:51:37 +0000

    MCSC-710 15/16 Period 2 PIA Input Screens (#147)

---
+++ app/forms/annualallowance/taxyear/PensionSchemeInput2016preAmountsFormProvider.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.annualallowance.taxyear
+
+import forms.mappings.Mappings
+import models.{PensionSchemeInput2016preAmounts, PensionSchemeInputAmounts}
+import play.api.data.Form
+import play.api.data.Forms._
+
+import javax.inject.Inject
+
+class PensionSchemeInput2016preAmountsFormProvider @Inject() extends Mappings {
+
+  def apply(): Form[PensionSchemeInput2016preAmounts] = Form(
+    mapping(
+      "originalPIA" -> bigInt(
+        "pensionSchemeInputAmounts.error.originalPIA.required",
+        "pensionSchemeInputAmounts.error.originalPIA.wholeNumber",
+        "pensionSchemeInputAmounts.error.originalPIA.nonNumeric"
+      )
+        .verifying(inRange[BigInt](0, BigInt("999999999"), "pensionSchemeInputAmounts.error.originalPIA.length")),
+      "revisedPIA"  -> bigInt(
+        "pensionSchemeInputAmounts.error.revisedPIA.required",
+        "pensionSchemeInputAmounts.error.revisedPIA.wholeNumber",
+        "pensionSchemeInputAmounts.error.revisedPIA.nonNumeric"
+      )
+        .verifying(inRange[BigInt](0, BigInt("999999999"), "pensionSchemeInputAmounts.error.revisedPIA.length"))
+    )(PensionSchemeInput2016preAmounts.apply)(PensionSchemeInput2016preAmounts.unapply)
+  )
+}
