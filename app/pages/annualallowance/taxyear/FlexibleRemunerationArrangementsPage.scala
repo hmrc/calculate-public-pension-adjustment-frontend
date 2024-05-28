d3688981ee777fefbcbb9222f9ff4a37423517a1
ElliotAndersonHMRC <114657325+ElliotAndersonHMRC@users.noreply.github.com>
2024-05-28 15:31:16 +0100

    MCSC-878 Create Salary Sacrifice/Flexi Remuneration Placeholders (3.16.1 - 3.16.5)

---
+++ app/pages/annualallowance/taxyear/FlexibleRemunerationArrangementsPage.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages
+
+import models.{CheckMode, NormalMode, Period, UserAnswers}
+import play.api.libs.json.JsPath
+import play.api.mvc.Call
+
+case class FlexibleRemunerationArrangementsPage(period: Period) extends QuestionPage[Boolean] {
+
+  override def path: JsPath = JsPath \ "aa" \ "years" \ period.toString \ toString
+
+  override def toString: String = "flexibleRemunerationArrangements"
+
+  override protected def navigateInNormalMode(answers: UserAnswers): Call =
+    answers.get(FlexibleRemunerationArrangementsPage(period)) match {
+      case Some(true)  =>
+        controllers.annualallowance.taxyear.routes.AmountFlexibleRemunerationArrangementsController
+          .onPageLoad(NormalMode, period)
+      case Some(false) =>
+        controllers.annualallowance.taxyear.routes.HowMuchContributionPensionSchemeController
+          .onPageLoad(NormalMode, period)
+      case _           => controllers.routes.JourneyRecoveryController.onPageLoad(None)
+    }
+
+  override protected def navigateInCheckMode(answers: UserAnswers): Call =
+    answers.get(FlexibleRemunerationArrangementsPage(period)) match {
+      case Some(true)  =>
+        controllers.annualallowance.taxyear.routes.AmountFlexibleRemunerationArrangementsController
+          .onPageLoad(CheckMode, period)
+      case Some(false) =>
+        controllers.annualallowance.taxyear.routes.HowMuchContributionPensionSchemeController
+          .onPageLoad(CheckMode, period)
+      case _           => controllers.routes.JourneyRecoveryController.onPageLoad(None)
+    }
+}
