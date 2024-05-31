9befc272f0e11e75aa920a46758f7cd52905ce96
Thomas Sarno <Thomassarno@hotmail.com>
2024-05-31 12:47:42 +0100

    MCSC-879, Create Lump Sum Death Benefits Placeholders (#221)

---
+++ app/pages/annualallowance/taxyear/AnyLumpSumDeathBenefitsPage.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.annualallowance.taxyear
+
+import controllers.annualallowance.taxyear.routes.{CheckYourAAPeriodAnswersController, LumpSumDeathBenefitsValueController}
+import models.{CheckMode, NormalMode, Period, UserAnswers}
+import pages.QuestionPage
+import play.api.libs.json.JsPath
+import play.api.mvc.Call
+
+case class AnyLumpSumDeathBenefitsPage(period: Period) extends QuestionPage[Boolean] {
+
+  override def path: JsPath = JsPath \ "aa" \ "years" \ period.toString \ toString
+
+  override def toString: String = "anyLumpSumDeathBenefits"
+
+  override protected def navigateInNormalMode(answers: UserAnswers): Call =
+    answers.get(AnyLumpSumDeathBenefitsPage(period)) match {
+      case Some(true)  => LumpSumDeathBenefitsValueController.onPageLoad(NormalMode, period)
+      case Some(false) => controllers.routes.JourneyRecoveryController.onPageLoad(None)
+      case None        => controllers.routes.JourneyRecoveryController.onPageLoad(None)
+    }
+
+  override protected def navigateInCheckMode(answers: UserAnswers): Call =
+    answers.get(AnyLumpSumDeathBenefitsPage(period)) match {
+      case Some(true)  => LumpSumDeathBenefitsValueController.onPageLoad(CheckMode, period)
+      case Some(false) => CheckYourAAPeriodAnswersController.onPageLoad(period)
+      case None        => controllers.routes.JourneyRecoveryController.onPageLoad(None)
+    }
+}
---
+++ test/forms/annualallowance/taxyear/AnyLumpSumDeathBenefitsFormProviderSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.annualallowance.taxyear
+
+import forms.behaviours.BooleanFieldBehaviours
+import models.Period
+import play.api.data.FormError
+
+class AnyLumpSumDeathBenefitsFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "anyLumpSumDeathBenefits.error.required"
+  val invalidKey  = "error.boolean"
+
+  val period = Period._2020
+  val form   = new AnyLumpSumDeathBenefitsFormProvider()(period)
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
