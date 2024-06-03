a7e77d1e855a6b2c96f085e2905344b90a4bba25
TJSarno <thomassarno@hotmail.com>
2024-06-03 15:57:48 +0100

    MCSC-877, added period to InterestFromSavings files

--- app/controllers/annualallowance/taxyear/InterestFromSavingsController.scala
+++ app/controllers/annualallowance/taxyear/InterestFromSavingsController.scala
@@ -1,13 +1,30 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.annualallowance.taxyear
 
 import controllers.actions._
 import forms.annualallowance.taxyear.InterestFromSavingsFormProvider
-import models.Mode
+import models.{Mode, Period}
 import pages.annualallowance.taxyear.InterestFromSavingsPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.UserDataService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.annualallowance.taxyear.InterestFromSavingsView
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
@@ -23,31 +40,33 @@ class InterestFromSavingsController @Inject()(
                                         view: InterestFromSavingsView
                                       )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+
+  def onPageLoad(mode: Mode, period: Period): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
+      val form = formProvider(period)
 
-      val preparedForm = request.userAnswers.get(InterestFromSavingsPage) match {
+      val preparedForm = request.userAnswers.get(InterestFromSavingsPage(period)) match {
         case None => form
         case Some(value) => form.fill(value)
       }
 
-      Ok(view(preparedForm, mode))
+      Ok(view(preparedForm, mode, period))
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(mode: Mode, period: Period): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
+      val form = formProvider(period)
 
       form.bindFromRequest().fold(
         formWithErrors =>
-          Future.successful(BadRequest(view(formWithErrors, mode))),
+          Future.successful(BadRequest(view(formWithErrors, mode, period))),
 
         value =>
           for {
-            updatedAnswers <- Future.fromTry(request.userAnswers.set(InterestFromSavingsPage, value))
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(InterestFromSavingsPage(period), value))
             _              <- userDataService.set(updatedAnswers)
-          } yield Redirect(InterestFromSavingsPage.navigate(mode, updatedAnswers))
+          } yield Redirect(InterestFromSavingsPage(period).navigate(mode, updatedAnswers))
       )
   }
 }
