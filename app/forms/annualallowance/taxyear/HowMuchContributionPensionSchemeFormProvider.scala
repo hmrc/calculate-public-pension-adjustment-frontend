091a337876c71a2ae33898d8084f8324bb9c925c
TJSarno <thomassarno@hotmail.com>
2024-06-10 15:38:12 +0100

    MCSC-921, added howmuchcont content

--- app/forms/annualallowance/taxyear/HowMuchContributionPensionSchemeFormProvider.scala
+++ app/forms/annualallowance/taxyear/HowMuchContributionPensionSchemeFormProvider.scala
@@ -18,18 +18,19 @@ package forms.annualallowance.taxyear
 
 import forms.mappings.Mappings
 import play.api.data.Form
+import play.api.i18n.Messages
 
 import javax.inject.Inject
 
 class HowMuchContributionPensionSchemeFormProvider @Inject() extends Mappings {
 
-  def apply(): Form[BigInt] =
+  def apply(startEndDate: String)(implicit messages: Messages): Form[BigInt] =
     Form(
-      "value" -> bigInt(
-        "howMuchContributionPensionScheme.error.required",
+      "value" -> bigInt(messages(
+        "howMuchContributionPensionScheme.error.required", startEndDate,
         "howMuchContributionPensionScheme.error.wholeNumber",
         "howMuchContributionPensionScheme.error.nonNumeric"
-      )
+      ))
         .verifying(inRange[BigInt](0, BigInt("999999999"), "howMuchContributionPensionScheme.error.outOfRange"))
     )
 }
