5e7a6dc667ba7deed0e418282e7ec405c1a299f5
TJSarno <thomassarno@hotmail.com>
2024-06-19 15:46:53 +0100

    MCSC-922, added tax relief content and updated test cases

--- app/viewmodels/checkAnswers/annualallowance/taxyear/TaxReliefSummary.scala
+++ app/viewmodels/checkAnswers/annualallowance/taxyear/TaxReliefSummary.scala
@@ -26,12 +26,21 @@ import utils.CurrencyFormatter.currencyFormat
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
 object TaxReliefSummary {
 
   def row(answers: UserAnswers, period: Period)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(TaxReliefPage(period)).map { answer =>
+
+      val languageTag          = if (messages.lang.code == "cy") "cy" else "en"
+      val formatter            = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.forLanguageTag(languageTag))
+      val startEndDate: String =
+        period.start.format(formatter) + " " + messages("startEndDateTo") + " " + period.end.format(formatter)
+
       SummaryListRowViewModel(
-        key = "taxRelief.checkYourAnswersLabel",
+        key = messages("taxRelief.checkYourAnswersLabel", startEndDate),
         value = ValueViewModel(HtmlContent(currencyFormat(answer))),
         actions = Seq(
           ActionItemViewModel("site.change", routes.TaxReliefController.onPageLoad(CheckMode, period).url)
