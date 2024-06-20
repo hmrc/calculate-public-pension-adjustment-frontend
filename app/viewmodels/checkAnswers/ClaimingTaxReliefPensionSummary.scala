084d77930e4ea32ceebc67076a49376aae445795
Thomas Sarno <Thomassarno@hotmail.com>
2024-06-20 10:58:00 +0100

    MCSC 922 Add Tax Relief Content (#243)

--- app/viewmodels/checkAnswers/ClaimingTaxReliefPensionSummary.scala
+++ app/viewmodels/checkAnswers/ClaimingTaxReliefPensionSummary.scala
@@ -24,14 +24,22 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
 
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
 object ClaimingTaxReliefPensionSummary {
 
   def row(answers: UserAnswers, period: Period)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(ClaimingTaxReliefPensionPage(period)).map { answer =>
       val value = if (answer) "site.yes" else "site.no"
 
+      val languageTag          = if (messages.lang.code == "cy") "cy" else "en"
+      val formatter            = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.forLanguageTag(languageTag))
+      val startEndDate: String =
+        period.start.format(formatter) + " " + messages("startEndDateTo") + " " + period.end.format(formatter)
+
       SummaryListRowViewModel(
-        key = "claimingTaxReliefPension.checkYourAnswersLabel",
+        key = messages("claimingTaxReliefPension.checkYourAnswersLabel", startEndDate),
         value = ValueViewModel(value),
         actions = Seq(
           ActionItemViewModel(
