5f5cf0afefafa9e135e51adef2226dfe7dfabe79
mrah93 <156314213+mrah93@users.noreply.github.com>
2024-08-09 16:30:14 +0100

    MCSC-1016 Move BCE page and add kickout status (#285)

--- test/forms/setupquestions/lifetimeallowance/HadBenefitCrystallisationEventFormProviderSpec.scala
+++ test/forms/setupquestions/lifetimeallowance/HadBenefitCrystallisationEventFormProviderSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package forms.lifetimeallowance
+package forms.setupquestions.lifetimeallowance
 
 import forms.behaviours.BooleanFieldBehaviours
 import play.api.data.FormError
