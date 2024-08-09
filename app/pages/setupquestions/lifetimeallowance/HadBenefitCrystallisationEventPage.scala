d4fca2c386ef2d08c7ce60fea26e3f96f731d7be
mrah93 <156314213+mrah93@users.noreply.github.com>
2024-08-09 16:30:14 +0100

    MCSC-1016 Move BCE page and add kickout status (#285)

--- app/pages/setupquestions/lifetimeallowance/HadBenefitCrystallisationEventPage.scala
+++ app/pages/setupquestions/lifetimeallowance/HadBenefitCrystallisationEventPage.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package pages.lifetimeallowance
+package pages.setupquestions.lifetimeallowance
 
 import controllers.setupquestions.lifetimeallowance.{routes => setupLTARoutes}
 import controllers.lifetimeallowance.{routes => ltaRoutes}
