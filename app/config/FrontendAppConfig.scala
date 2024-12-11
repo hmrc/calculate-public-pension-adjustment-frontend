6b2aff7a93808e03ac784f51cdad7b0c1500128e
javed-iqbal1 <131012491+javed-iqbal1@users.noreply.github.com>
2024-12-11 12:10:22 +0000

    MCSC-1126 turn welsh flag off

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -63,7 +63,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
 //  val languageTranslationEnabled: Boolean =
 //    configuration.get[Boolean]("features.welsh-translation")
 
-  val languageTranslationEnabled: Boolean = true
+  val languageTranslationEnabled: Boolean = false
 
   val origin = configuration.get[String]("origin")
 
