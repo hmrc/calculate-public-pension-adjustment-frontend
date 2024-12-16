74f5a903a9a9f0428a7d75eb2b60c0c7cc073451
Michael Abbott <112705928+MichaelAAbbott@users.noreply.github.com>
2024-12-16 17:15:07 +0000

    MCSC-1205 New page to address bookmarking issue on calc results page (#383)

--- test/controllers/CalculationReviewIndividualAAControllerSpec.scala
+++ test/controllers/CalculationReviewIndividualAAControllerSpec.scala
@@ -17,22 +17,27 @@
 package controllers
 
 import base.SpecBase
+import config.FrontendAppConfig
 import models.CalculationResults.{CalculationResponse, CalculationReviewIndividualAAViewModel, IndividualAASummaryModel, RowViewModel}
 import models.Period
 import models.submission.{Failure, Success}
-import models.tasklist.sections.LTASection
+import models.tasklist.SectionStatus.{Completed, InProgress, NotStarted}
+import models.tasklist.{SectionGroupViewModel, SectionStatus, SectionViewModel, TaskListViewModel}
+import models.tasklist.sections.{LTASection, NextStepsSection}
 import models.tasklist.sections.LTASection.cannotUseLtaServiceNoChargePage
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
+import pages.annualallowance.preaaquestions.StopPayingPublicPensionPage
 import play.api.inject.bind
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Call, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{route, status, _}
-import services.CalculationResultService
+import services.{CalculationResultService, TaskListService}
 import uk.gov.hmrc.http.HeaderCarrier
 
+import java.time.LocalDate
 import scala.concurrent.Future
 import scala.io.Source
 
@@ -44,9 +49,101 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
 
   lazy val normalRouteOutDate = routes.CalculationReviewIndividualAAController.onPageLoad(Period._2016).url
   lazy val normalRouteInDate  = routes.CalculationReviewIndividualAAController.onPageLoad(Period._2022).url
-  lazy val submitRoute        = routes.CalculationReviewIndividualAAController.onSubmit().url
+  lazy val submitRoute        = routes.CalculationReviewIndividualAAController.onSubmit(Period._2020).url
 
   "CalculationReviewIndividualAA Controller" - {
+
+    "must redirect to outstanding tasks page on a GET if next steps section is not ready to start" in {
+
+      val application = applicationBuilder(Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, normalRouteOutDate)
+
+        val result = route(application, request).value
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.redirectToOutstandingTasksPage
+      }
+    }
+
+    "must redirect to outstanding tasks page on a POST if next steps section is not ready to start" in {
+
+      val application = applicationBuilder(Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, normalRouteOutDate)
+
+        val result = route(application, request).value
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.redirectToOutstandingTasksPage
+      }
+    }
+
+    "must redirect to task list when user accessing a period which they have not indicated on a GET" in {
+
+      val mockTaskListService = mock[TaskListService]
+
+      when(mockTaskListService.taskListViewModel(any())).thenReturn(
+        TaskListViewModel(
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+          None,
+          None,
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+        )
+      )
+
+      val userAnswers = emptyUserAnswers.set(StopPayingPublicPensionPage, LocalDate.of(2015, 7, 1)).success.value
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswers))
+          .overrides(
+            bind[TaskListService].toInstance(mockTaskListService)
+          )
+          .build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, normalRouteInDate)
+
+        val result = route(application, request).value
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.redirectToTaskListPage
+      }
+    }
+
+    "must redirect to task list when user accessing a period which they have not indicated on a POST" in {
+
+      val mockTaskListService = mock[TaskListService]
+
+      when(mockTaskListService.taskListViewModel(any())).thenReturn(
+        TaskListViewModel(
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+          None,
+          None,
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+        )
+      )
+
+      val userAnswers = emptyUserAnswers.set(StopPayingPublicPensionPage, LocalDate.of(2015, 7, 1)).success.value
+
+      val application =
+        applicationBuilder(userAnswers = Some(userAnswers))
+          .overrides(
+            bind[TaskListService].toInstance(mockTaskListService)
+          )
+          .build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(POST, normalRouteInDate)
+
+        val result = route(application, request).value
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual appConfig.redirectToTaskListPage
+      }
+    }
 
     "must show the calculation review individual AA view on a GET for out date" - {
 
@@ -56,6 +153,7 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
           readCalculationResult("test/resources/CalculationResultsTestData.json")
 
         val mockRowViewModel                           = RowViewModel("test", "test")
+        val mockTaskListService                        = mock[TaskListService]
         val mockCalculationResultService               = mock[CalculationResultService]
         val mockCalculationReviewIndividualAAViewModel =
           CalculationReviewIndividualAAViewModel(Seq(Seq(mockRowViewModel)), Seq(Seq(mockRowViewModel)))
@@ -78,13 +176,23 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
         when(mockCalculationResultService.calculationReviewIndividualAAViewModel(any, any, any)(any, any))
           .thenReturn(Future.successful(mockCalculationReviewIndividualAAViewModel))
 
+        when(mockTaskListService.taskListViewModel(any())).thenReturn(
+          TaskListViewModel(
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+            None,
+            None,
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+          )
+        )
+
         when(mockCalculationResultService.individualAASummaryModel(calculationResult))
           .thenReturn(Seq(mockIndividualAASummaryModel))
 
         val application =
           applicationBuilder(userAnswers = Some(emptyUserAnswers))
             .overrides(
-              bind[CalculationResultService].toInstance(mockCalculationResultService)
+              bind[CalculationResultService].toInstance(mockCalculationResultService),
+              bind[TaskListService].toInstance(mockTaskListService)
             )
             .build()
 
@@ -105,6 +213,7 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
           readCalculationResult("test/resources/CalculationResultsTestData.json")
 
         val mockRowViewModel                           = RowViewModel("test", "test")
+        val mockTaskListService                        = mock[TaskListService]
         val mockCalculationResultService               = mock[CalculationResultService]
         val mockCalculationReviewIndividualAAViewModel =
           CalculationReviewIndividualAAViewModel(Seq(Seq(mockRowViewModel)), Seq(Seq(mockRowViewModel)))
@@ -127,13 +236,23 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
         when(mockCalculationResultService.calculationReviewIndividualAAViewModel(any, any, any)(any, any))
           .thenReturn(Future.successful(mockCalculationReviewIndividualAAViewModel))
 
+        when(mockTaskListService.taskListViewModel(any())).thenReturn(
+          TaskListViewModel(
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+            None,
+            None,
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+          )
+        )
+
         when(mockCalculationResultService.individualAASummaryModel(calculationResult))
           .thenReturn(Seq(mockIndividualAASummaryModel))
 
         val application =
           applicationBuilder(userAnswers = Some(emptyUserAnswers))
             .overrides(
-              bind[CalculationResultService].toInstance(mockCalculationResultService)
+              bind[CalculationResultService].toInstance(mockCalculationResultService),
+              bind[TaskListService].toInstance(mockTaskListService)
             )
             .build()
 
@@ -160,6 +279,7 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
           readCalculationResult("test/resources/CalculationResultsTestData.json")
 
         val mockRowViewModel                           = RowViewModel("test", "test")
+        val mockTaskListService                        = mock[TaskListService]
         val mockCalculationResultService               = mock[CalculationResultService]
         val mockCalculationReviewIndividualAAViewModel =
           CalculationReviewIndividualAAViewModel(Seq(Seq(mockRowViewModel)), Seq(Seq(mockRowViewModel)))
@@ -182,13 +302,23 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
         when(mockCalculationResultService.calculationReviewIndividualAAViewModel(any, any, any)(any, any))
           .thenReturn(Future.successful(mockCalculationReviewIndividualAAViewModel))
 
+        when(mockTaskListService.taskListViewModel(any())).thenReturn(
+          TaskListViewModel(
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+            None,
+            None,
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+          )
+        )
+
         when(mockCalculationResultService.individualAASummaryModel(calculationResult))
           .thenReturn(Seq(mockIndividualAASummaryModel))
 
         val application =
           applicationBuilder(userAnswers = Some(emptyUserAnswers))
             .overrides(
-              bind[CalculationResultService].toInstance(mockCalculationResultService)
+              bind[CalculationResultService].toInstance(mockCalculationResultService),
+              bind[TaskListService].toInstance(mockTaskListService)
             )
             .build()
 
@@ -209,6 +339,7 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
           readCalculationResult("test/resources/CalculationResultsTestData.json")
 
         val mockRowViewModel                           = RowViewModel("test", "test")
+        val mockTaskListService                        = mock[TaskListService]
         val mockCalculationResultService               = mock[CalculationResultService]
         val mockCalculationReviewIndividualAAViewModel =
           CalculationReviewIndividualAAViewModel(Seq(Seq(mockRowViewModel)), Seq(Seq(mockRowViewModel)))
@@ -231,13 +362,23 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
         when(mockCalculationResultService.calculationReviewIndividualAAViewModel(any, any, any)(any, any))
           .thenReturn(Future.successful(mockCalculationReviewIndividualAAViewModel))
 
+        when(mockTaskListService.taskListViewModel(any())).thenReturn(
+          TaskListViewModel(
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+            None,
+            None,
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+          )
+        )
+
         when(mockCalculationResultService.individualAASummaryModel(calculationResult))
           .thenReturn(Seq(mockIndividualAASummaryModel))
 
         val application =
           applicationBuilder(userAnswers = Some(emptyUserAnswers))
             .overrides(
-              bind[CalculationResultService].toInstance(mockCalculationResultService)
+              bind[CalculationResultService].toInstance(mockCalculationResultService),
+              bind[TaskListService].toInstance(mockTaskListService)
             )
             .build()
 
@@ -258,6 +399,7 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
           readCalculationResult("test/resources/CalculationResultsTestData.json")
 
         val mockRowViewModel                           = RowViewModel("test", "test")
+        val mockTaskListService                        = mock[TaskListService]
         val mockCalculationResultService               = mock[CalculationResultService]
         val mockCalculationReviewIndividualAAViewModel =
           CalculationReviewIndividualAAViewModel(Seq(Seq(mockRowViewModel)), Seq(Seq(mockRowViewModel)))
@@ -280,13 +422,23 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
         when(mockCalculationResultService.calculationReviewIndividualAAViewModel(any, any, any)(any, any))
           .thenReturn(Future.successful(mockCalculationReviewIndividualAAViewModel))
 
+        when(mockTaskListService.taskListViewModel(any())).thenReturn(
+          TaskListViewModel(
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+            None,
+            None,
+            SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+          )
+        )
+
         when(mockCalculationResultService.individualAASummaryModel(calculationResult))
           .thenReturn(Seq(mockIndividualAASummaryModel))
 
         val application =
           applicationBuilder(userAnswers = Some(emptyUserAnswers))
             .overrides(
-              bind[CalculationResultService].toInstance(mockCalculationResultService)
+              bind[CalculationResultService].toInstance(mockCalculationResultService),
+              bind[TaskListService].toInstance(mockTaskListService)
             )
             .build()
 
@@ -304,8 +456,22 @@ class CalculationReviewIndividualAAControllerSpec extends SpecBase with MockitoS
 
     "must redirect to the CalculationReviewController on a POST" in {
 
+      val mockTaskListService = mock[TaskListService]
+      when(mockTaskListService.taskListViewModel(any())).thenReturn(
+        TaskListViewModel(
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", Completed, "", None))),
+          None,
+          None,
+          SectionGroupViewModel("", Seq(SectionViewModel("", "", NotStarted, "", None)))
+        )
+      )
+
       val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[TaskListService].toInstance(mockTaskListService)
+          )
+          .build()
 
       running(application) {
         val request =
