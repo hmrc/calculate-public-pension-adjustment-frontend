678eb4d588fcf7829c32aaee6d505c8dbb3f5733
michaelaabbott <112705928+MichaelAAbbott@users.noreply.github.com>
2025-02-12 10:18:16 +0000

    MCSC-1296 Enabled Welsh translations

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -61,11 +61,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val cppaBaseUrl: String =
     configuration.get[Service]("microservice.services.calculate-public-pension-adjustment").baseUrl
 
-//  TODO - Remove to add back welsh translantion
-//  val languageTranslationEnabled: Boolean =
-//    configuration.get[Boolean]("features.welsh-translation")
-
-  val languageTranslationEnabled: Boolean = false
+  val languageTranslationEnabled: Boolean =
+    configuration.get[Boolean]("features.welsh-translation")
 
   val origin = configuration.get[String]("origin")
 
