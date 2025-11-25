5ad5f138a7e13bd3e53b98ad218d3a6bf03f6117
Jessica Kay <114414761+jesskay2@users.noreply.github.com>
2025-11-25 11:14:23 +0000

    DDCNL-11274 | Migrate to service navigation (#459)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay30Version = "10.3.0"
+  private val bootstrapPlay30Version = "10.4.0"
   private val mongoPlay30Version     = "2.10.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.17.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.21.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % mongoPlay30Version,
@@ -22,7 +22,6 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalacheck-1-18"         % "3.2.19.0" % Test,
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.17.0" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2" % Test,
-    "org.pegdown"             % "pegdown"                 % "1.6.0" % Test,
     "org.jsoup"               % "jsoup"                   % "1.21.2" % Test,
     "org.playframework"      %% "play-test"               % PlayVersion.current % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlay30Version % Test,
