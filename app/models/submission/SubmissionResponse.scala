f917ac39211e1d0b7b21930f4a3b5f4f719600ed
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2026-01-29 16:56:58 +0000

    Dependancy upgrade

--- app/models/submission/SubmissionResponse.scala
+++ app/models/submission/SubmissionResponse.scala
@@ -16,7 +16,7 @@
 
 package models.submission
 
-import play.api.libs.json.{Json, OFormat}
+import play.api.libs.json.{Json, OFormat, OWrites, Reads, JsObject, JsError}
 
 sealed trait SubmissionResponse extends Product with Serializable
 
@@ -36,5 +36,20 @@ object Failure {
 }
 
 object SubmissionResponse {
-  implicit lazy val format: OFormat[SubmissionResponse] = Json.format[SubmissionResponse]
+  implicit lazy val writes: OWrites[SubmissionResponse] = OWrites {
+    case success: Success => Json.toJsObject(success)
+    case failure: Failure => Json.toJsObject(failure)
+  }
+
+  implicit lazy val reads: Reads[SubmissionResponse] = Reads { jsValue =>
+    jsValue.asOpt[JsObject] match {
+      case Some(obj) if (obj \ "uniqueId").isDefined =>
+        Success.format.reads(jsValue)
+      case Some(obj) if (obj \ "errors").isDefined =>
+        Failure.format.reads(jsValue)
+      case _ => JsError("Invalid SubmissionResponse")
+    }
+  }
+
+  implicit lazy val format: OFormat[SubmissionResponse] = OFormat(reads, writes)
 }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlay30Version = "10.4.0"
-  private val mongoPlay30Version     = "2.10.0"
+  private val bootstrapPlay30Version = "10.5.0"
+  private val mongoPlay30Version     = "2.12.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.21.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.29.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapPlay30Version,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % mongoPlay30Version,
@@ -17,15 +17,12 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapPlay30Version % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.19" % Test,
-    "org.scalacheck"         %% "scalacheck"              % "1.19.0" % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"         % "3.2.19.0" % Test,
     "org.scalatestplus"      %% "mockito-4-11"            % "3.2.17.0" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2" % Test,
     "org.jsoup"               % "jsoup"                   % "1.21.2" % Test,
     "org.playframework"      %% "play-test"               % PlayVersion.current % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoPlay30Version % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.64.8" % Test
   )
 
   val itDependencies = Seq(
